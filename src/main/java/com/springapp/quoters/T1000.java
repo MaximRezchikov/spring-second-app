0bef2efb33ac3dbd62d639706c34b5a1cd304d28
Maksim_Rezchikov <Maksim_Rezchikov@epam.com>
2019-11-11 19:47:58 +0300

    SSA-5commit BeanFactoryPostProcessor

---
+++ src/main/java/com/springapp/quoters/T1000.java
@@ -0,0 +1,9 @@
+package com.springapp.quoters;
+
+public class T1000  extends TerminatorQuoter implements Quoter {
+
+    @Override
+    public void sayQuote() {
+        System.out.println("Я ЖИДКИЙ");
+    }
+}
